f183879e5369ead26db208ba4aede90b2f75d04c
vinodreddyb <vinodbatthula@gmail.com>
2015-08-06 11:06:20 +0530

    nms deloyed

---
+++ src/main/java/org/vinod/Main.java
@@ -0,0 +1,24 @@
+package org.vinod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+@SpringBootApplication
+
+public class Main extends SpringBootServletInitializer  {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(new Class[] { Main.class});
+    }
+
+    
+   
+}
+
